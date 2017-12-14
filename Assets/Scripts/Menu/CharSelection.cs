b7261ed73ae1c5ae374384902a862b2050973ef1
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-14 02:33:02 -0200

    Fixing inputmanager for joysticks, starting menu implementation

---
+++ Assets/Scripts/Menu/CharSelection.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharSelection : MonoBehaviour {
+
+    public int maxPlayers = 2;
+    public InputSource[] sources;
+    public int currentPlayer = 1;
+
+    void Start() {
+        sources = new InputSource[maxPlayers];
+        for (int i = 0; i < maxPlayers; ++i)
+            sources[i] = (InputSource)PlayerPrefs.GetInt("Player" + (i+1).ToString() + "InputSource");
+    }
+
+    void Update() {
+        if (currentPlayer < maxPlayers) {
+            for (int i = 0; i < 5; ++i) {
+                if (InputManager.ShootButton((InputSource)i)) {
+                    bool alreadyUsed = false;
+                    for (int k = 0; k < currentPlayer; ++k) {
+                        if ((int)sources[k] == i)
+                            alreadyUsed = true;
+                    }
+                    if (!alreadyUsed) {
+                        sources[currentPlayer] = (InputSource) i;
+                        currentPlayer++;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Menu/StartMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public string charSelectionMenu;
+
+    bool requestedStart = false;
+
+    void Start() {
+        for (int i = 0; i < 5; ++i) {
+            PlayerPrefs.SetInt("Player" + (i+1).ToString() + "InputSource", -1);
+        }
+    }
+
+    void Update() {
+        if (!requestedStart) {
+            for (int i = 0; i < 5; ++i) {
+                if (InputManager.ShootButton((InputSource)i)) {
+                    PlayerPrefs.SetInt("Player1InputSource", i);
+                    requestedStart = true;
+                    SceneManager.LoadScene(charSelectionMenu);
+                    break;
+                }
+            }
+        }
+    }
+}
