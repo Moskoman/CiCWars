62fb78fdd545cd4f70bb180d5339f007f44bbca6
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-14 00:47:35 -0200

    Updating collisions and adding ZAxisShift

---
+++ Assets/Scripts/ZAxisShift.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ZAxisShift : MonoBehaviour {
+
+    void FixedUpdate() {
+        var position = transform.position;
+        position = new Vector3(position.x, position.y, position.y);
+        transform.position = position;
+    }
+}
