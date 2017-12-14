62cd3bee90cb89160e5a493b2b5c94d2f82582fd
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-14 09:28:11 -0200

    Implemented end game conditions

---
+++ Assets/Scripts/EndGameConditions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGameConditions : MonoBehaviour {
+
+    void Update() {
+        int livePlayers = 0;
+        int index = -1;
+        for (int i = 0; i < PlayerSpawner.instance.playerAmount; ++i) {
+            if (PlayerSpawner.instance.players[i].GetComponent<Player>().life > 0) {
+                livePlayers++;
+                index = i;
+            }
+        }
+        if (livePlayers == 1) {
+            Debug.Log("Player " + (index+1) + " wins!");
+        }
+    }
+}
